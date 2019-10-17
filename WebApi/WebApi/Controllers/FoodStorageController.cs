6acfbad37f0aa832831fc608fbb4727c73723f18
zanuccod <54598430+zanuccod@users.noreply.github.com>
2019-10-17 23:07:52 +0200

    Add WebApi + WebApiTest projects

---
+++ WebApi/WebApi/Controllers/FoodStorageController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using FoodStorage.Entities;
+using FoodStorage.Models;
+using FoodStorage.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FoodStorageController : ControllerBase
+    {
+        private readonly IPackService packService;
+
+        public FoodStorageController(IPackService packService)
+        {
+            this.packService = packService;
+        }
+
+        [HttpGet("GetAllPack")]
+        public List<Pack> Get()
+        {
+            return packService.GetPackList();
+        }
+    }
+}
