1e1cf3952e0ee574124e14985120173beb22bb07
zanuccod <54598430+zanuccod@users.noreply.github.com>
2019-10-19 18:43:34 +0200

    Add test methods for GetPack end point

---
+++ WebApi/WebApiTest/Controller/FoodStorageControllerTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Xunit;
+using WebApi.Controllers;
+using Moq;
+using FoodStorage.Services;
+using FoodStorage.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiTest.Controller
+{
+    public class FoodStorageControllerTest
+    {
+        Mock<IPackService> mockPackService;
+        private IPackService service;
+        private FoodStorageController controller;
+
+        [Fact]
+        public void GetPack_PackNotFound_ShouldReturnNotFound()
+        {
+            // Arrange
+            mockPackService = new Mock<IPackService>();
+            service = mockPackService.Object;
+            controller = new FoodStorageController(service);
+
+            var packId = 1;
+            mockPackService.Setup(x => x.GetPack(packId)).Returns<Pack>(null);
+
+            // Act
+            var result = controller.GetPack(packId);
+
+            // Assert
+            Assert.IsType<NotFoundObjectResult>(result.Result);
+        }
+
+        [Fact]
+        public void GetPack_PackFound_ShouldReturnPack()
+        {
+            // Arrange
+            mockPackService = new Mock<IPackService>();
+            service = mockPackService.Object;
+            controller = new FoodStorageController(service);
+
+            var packId = 1;
+            mockPackService.Setup(x => x.GetPack(packId)).Returns(new Pack() { Id = 1 });
+
+            // Act
+            var result = controller.GetPack(packId);
+
+            // Assert
+            Assert.IsType<Pack>(result.Value);
+            Assert.True(result.Value.Id == packId);
+        }
+    }
+}
