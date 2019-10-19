caf49bedbb6da7e9318b19bac32dd5a6d77e83d9
zanuccod <54598430+zanuccod@users.noreply.github.com>
2019-10-19 21:30:29 +0200

    Update Program to listen always on http://localhost:5000 address

--- WebApi/WebApi/Program.cs
+++ WebApi/WebApi/Program.cs
@@ -20,7 +20,9 @@ namespace WebApi
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                    .UseStartup<Startup>()
+                    .UseUrls("http://localhost:5000/");
                 });
     }
 }
