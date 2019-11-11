bc3c9aeadd9dc7d0eae508ee3d1e917615754bf2
zanuccod <54598430+zanuccod@users.noreply.github.com>
2019-11-11 22:05:47 +0100

    Test sonarcloud

--- FoodStorage/FoodStorage/Models/EFDataModel.cs
+++ FoodStorage/FoodStorage/Models/EFDataModel.cs
@@ -28,36 +28,46 @@ namespace FoodStorage.Models
 
         public async Task<List<Pack>> GetPackList()
         {
-            using var db = new EFDataContext(options);
-            return await db.Packs.ToListAsync().ConfigureAwait(true);
+            using (var db = new EFDataContext(options))
+            {
+                return await db.Packs.ToListAsync().ConfigureAwait(true);
+            }
         }
 
         public async Task<Pack> GetPack(long id)
         {
-            using var db = new EFDataContext(options);
-            return await db.Packs.FindAsync(id).ConfigureAwait(true);
+            using (var db = new EFDataContext(options))
+            {
+                return await db.Packs.FindAsync(id).ConfigureAwait(true);
+            }
         }
 
         public async Task InsertPack(Pack obj)
         {
-            using var db = new EFDataContext(options);
-            await db.Packs.AddAsync(obj);
-            await db.SaveChangesAsync().ConfigureAwait(true);
+            using (var db = new EFDataContext(options))
+            {
+                await db.Packs.AddAsync(obj);
+                await db.SaveChangesAsync().ConfigureAwait(true);
+            }
         }
 
         public async Task UpdatePack(Pack obj)
         {
-            using var db = new EFDataContext(options);
-            db.Packs.Update(obj);
-            await db.SaveChangesAsync().ConfigureAwait(true);
+            using (var db = new EFDataContext(options))
+            {
+                db.Packs.Update(obj);
+                await db.SaveChangesAsync().ConfigureAwait(true);
+            }
         }
 
         public async Task DeletePack(long id)
         {
-            using var db = new EFDataContext(options);
-            var item = await db.Packs.FindAsync(id);
-            db.Packs.Remove(item);
-            await db.SaveChangesAsync().ConfigureAwait(true);
+            using (var db = new EFDataContext(options))
+            {
+                var item = await db.Packs.FindAsync(id);
+                db.Packs.Remove(item);
+                await db.SaveChangesAsync().ConfigureAwait(true);
+            }
         }
 
         #endregion
--- WebApi/WebApi/Startup.cs
+++ WebApi/WebApi/Startup.cs
@@ -27,15 +27,15 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            //services.AddControllers();
 
             var model = new EFDataModel();
             services.AddSingleton<IPackService>(new PackService(model));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {/*
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -50,7 +50,7 @@ namespace WebApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-            });
+            });*/
         }
     }
 }
